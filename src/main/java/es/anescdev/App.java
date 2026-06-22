fdd9e320b99d5d3b140543d86439bf51100f37b5
anescdev <anescdev@gmail.com>
2026-06-22 02:38:11 +0200

    feat: include register data persister

--- src/main/java/es/anescdev/App.java
+++ src/main/java/es/anescdev/App.java
@@ -6,6 +6,9 @@ import java.util.ResourceBundle;
 
 import org.codejargon.feather.Feather;
 
+import com.j256.ormlite.field.DataPersisterManager;
+
+import es.anescdev.shared.infrastructure.persistence.DurationDataType;
 import es.anescdev.sumatory.SumatoryModule;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -21,13 +24,13 @@ public class App extends Application {
 	private static Stage main;
 	private static App instance;
 	private Feather feather;
-	
-	
+
 	@Override
 	public void init() throws Exception {
 		App.resourceBundle = ResourceBundle.getBundle("i18n/messages", Locale.getDefault());
 		App.instance = this;
 		this.feather = Feather.with(new AppModule(this.getParameters()), new SumatoryModule());
+		DataPersisterManager.registerDataPersisters(DurationDataType.getSingleton());
 		super.init();
 	}
 
@@ -47,32 +50,39 @@ public class App extends Application {
 		primaryStage.toFront();
 		primaryStage.show();
 	}
+
 	/**
 	 * Carga un fichero FXML para poder tratarlo dentro del programa
-	 * @param <T> tipo del elemento raiz del FXML cargado
-	 * @param resourcePath ruta al recurso FXML. Importante que esté dentro de la carpeta de recurso
+	 * 
+	 * @param <T>          tipo del elemento raiz del FXML cargado
+	 * @param resourcePath ruta al recurso FXML. Importante que esté dentro de la
+	 *                     carpeta de recurso
 	 * @return Nodo de JavaFX que ha obtenido en el FXML
-	 * @throws LoadFXMLException 
+	 * @throws LoadFXMLException
 	 */
-	public static <T>  T loadFXML(String resourcePath) throws LoadFXMLException {
-		if (App.resourceBundle == null) throw new RuntimeException("The resource bundle cannot be loaded");
+	public static <T> T loadFXML(String resourcePath) throws LoadFXMLException {
+		if (App.resourceBundle == null)
+			throw new RuntimeException("The resource bundle cannot be loaded");
 		try {
-			FXMLLoader loader = new FXMLLoader(App.class.getClassLoader().getResource(String.format("scenes/%s.fxml", resourcePath)), App.resourceBundle);
+			FXMLLoader loader = new FXMLLoader(
+					App.class.getClassLoader().getResource(String.format("scenes/%s.fxml", resourcePath)),
+					App.resourceBundle);
 			loader.setControllerFactory(type -> App.instance().feather.instance(type));
 			return loader.load();
 		} catch (IOException exception) {
 			throw new LoadFXMLException(exception.getLocalizedMessage(), exception.getCause());
 		}
 	}
-	
+
 	public static App instance() {
 		return App.instance;
 	}
+
 	public static Stage getMainStage() {
 		return App.main;
 	}
-	
+
 	public static void main(String[] args) {
-        App.launch(args);
-    }
+		App.launch(args);
+	}
 }
